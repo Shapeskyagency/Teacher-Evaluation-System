3fb7e52e30c533722d8263b5e91ee564a82a01f3
SHEKHAR SHARMA <112850260+TESHEKHAR@users.noreply.github.com>
2024-11-12 15:05:56 +0530

    get all users and change password

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -86,4 +86,29 @@ const resetPassword = async (req, res) => {
     res.json({ message: 'Password has been reset successfully' });
 };
 
-module.exports = { register, login,requestPasswordReset, resetPassword }; 
+const changePassword = async (req, res) => {
+    const { id } = req.user;
+    const { currentPassword, newPassword } = req.body;
+
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Current password is incorrect' });
+        }
+
+        user.password = newPassword;
+        await user.save();
+
+        res.status(200).json({ message: 'Password changed successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+module.exports = { register, login,requestPasswordReset, resetPassword, changePassword }; 
