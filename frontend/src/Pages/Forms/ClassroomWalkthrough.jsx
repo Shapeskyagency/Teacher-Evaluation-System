2cad5ae072f4ca8371329a29886dbd6532129706
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2024-11-29 19:15:29 +0530

    testing

--- frontend/src/Pages/Forms/ClassroomWalkthrough.jsx
+++ frontend/src/Pages/Forms/ClassroomWalkthrough.jsx
@@ -2,14 +2,16 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function ClassroomWalkthrough() {
+  const navigate = useNavigate()
   return (
     <div className="container py-4">
       <Row>
         <Col>
           <Button
-            // onClick={() => navigate("/fortnightly-monitor/create")}
+            onClick={() => navigate("/classroom-walkthrough/create")}
             type="primary"
             icon={<PlusCircleOutlined />}
             size="large"
