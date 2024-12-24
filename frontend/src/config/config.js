2303de9827ca79b07205cc48067770fcc3776c0e
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2024-12-25 04:25:26 +0530

    Fixed

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports={
-    baseURL:"http://localhost:5000/api/",
+    // baseURL:"http://localhost:5000/api/",
     // baseURL:"https://dekhooo.com/api/",
     // baseURL:"http://148.135.136.129/api",
-    // baseURL:"https://teacher-evaluation-system-t4rh.onrender.com/api",
+    baseURL:"https://teacher-evaluation-system-t4rh.onrender.com/api",
     UserRole:["Superadmin","Observer","Teacher"]
 }
