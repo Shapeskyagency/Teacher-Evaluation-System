d277b3c3a7364225ae08dfca3b0ceffc94aa982b
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2025-02-13 15:26:09 +0530

    New Chnages

--- frontend/src/Utils/TeacherLayout.js
+++ frontend/src/Utils/TeacherLayout.js
@@ -47,9 +47,9 @@ const TeacherLayout = () => {
         <Content style={contentStyle}>
         <Outlet />
         </Content>
-        <Footer style={footerStyle}>
+        {/* <Footer style={footerStyle}>
           <MainFooter/>
-        </Footer>
+        </Footer> */}
       </Layout>
     </Layout>
     </>
